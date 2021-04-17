3964fec3d67ce42bcc052385cd3aee7ed950bcab
Paras <parastehria@gmail.com>
2021-04-17 23:58:35 +0530

    main: logging level info

--- run_squad.py
+++ run_squad.py
@@ -1131,7 +1131,7 @@ def validate_flags_or_throw(bert_config):
 
 
 def main(_):
-    tf.logging.set_verbosity(tf.logging.ERROR)
+    tf.logging.set_verbosity(tf.logging.INFO)
 
     bert_config = modeling.BertConfig.from_json_file(FLAGS.bert_config_file)
 
